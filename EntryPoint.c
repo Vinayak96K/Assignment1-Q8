10444af64f9cad4405b21a55ce5d95e5121beceb
Vinayak96k <vinayak.patil0304@gmail.com>
2018-08-29 15:35:19 +0530

    first commit

---
+++ EntryPoint.c
@@ -0,0 +1,11 @@
+#include"MyHeader.h"
+
+int main(int argc, char const *argv[])
+{
+    char cAlpha;
+    printf("Enter a charachter:");
+    scanf("%c",&cAlpha);
+
+    Display(cAlpha);
+    return 0;
+}
